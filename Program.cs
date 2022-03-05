22a07a68e1c1715a6b5f78280af2d5acc9721083
Anna Beatriz <annab_oliver@hotmail.com>
2022-03-04 22:16:26 -0300

    Finaliza Exercicio

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿//exibe título na cor certa
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Help!");
+
+//Volta a cor padrão
+Console.ResetColor();
+
+Console.WriteLine("I need somebody");
+Console.WriteLine("(Help) not just anybody");
+Console.WriteLine("(Help) you know I need someone, help");
+Console.WriteLine("So much younger than today");
+Console.WriteLine("(I never need) I never needed anybody's help in any way");
+Console.WriteLine("(Now) but now these days are gone (these days are gone)");
+Console.WriteLine("I'm not so self assured");
+Console.WriteLine("(And now I find) now I find I've changed my mind");
+Console.WriteLine("And opened up the doors");
+
+//Muda cor refrão
+Console.ForegroundColor= ConsoleColor.Blue;
+Console.WriteLine("Help me if you can, I'm feeling down");
+Console.WriteLine("And I do appreciate you being 'round");
+Console.WriteLine("Help me get my feet back on the ground");
+Console.WriteLine("Won't you please, please help me");
+
+//Volta a cor padrão
+Console.ResetColor();
+Console.WriteLine("In oh so many ways");
+Console.WriteLine("(My independ-) my independence seems to vanish in the haze");
+Console.WriteLine("(But) but every now and then (now and then)");
+Console.WriteLine("I feel so insecure");
+Console.WriteLine("(I know that I) I know that I just need you like");
+Console.WriteLine("I've never done before");
+
+//Muda a cor do refrão
+Console.ForegroundColor= ConsoleColor.Blue;
+Console.WriteLine("Help me if you can, I'm feeling down");
+Console.WriteLine("And I do appreciate you being 'round");
+Console.WriteLine("Help me get my feet back on the ground");
+Console.WriteLine("Won't you please, please help me");
+
+//volta acor padrão
+Console.ResetColor();
+
+Console.WriteLine("When I was younger, so much younger than today");
+Console.WriteLine("I never needed anybody's help in any way");
+Console.WriteLine("(Now) but now these days are gone (these days are gone)");
+Console.WriteLine("I'm not so self assured");
+Console.WriteLine("(And now I find) now I find I've changed my mind");
+Console.WriteLine("And opened up the doors");
+Console.WriteLine("Help me if you can, I'm feeling down");
+Console.WriteLine("And I do appreciate you being 'round");
+Console.WriteLine("Help me get my feet back on the ground");
+Console.WriteLine("Won't you please, please help me, help me, help me, ooh");
+
